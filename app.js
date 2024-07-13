d8df58f7308c72db288ed9c7feb677424dbd63ca
Denis Onica <denis.onica97@gmail.com>
2024-07-13 13:56:49 +0300

    updated cors

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://airclaims-frontend1.onrender.com",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(compression());
